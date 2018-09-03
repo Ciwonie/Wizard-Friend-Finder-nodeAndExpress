10186f1cff7a6b412d2e6ac8af2ecadcf927c4b2
Ciwonie <ciwonie@gmail.com>
2018-09-03 13:34:34 -0500

    completed wizarding app

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [ {
+    "name" : "Harry Potter",
+    "photo" : "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2014%2F07%2F301386_full1.jpg",
+    "scores" : [3,4,5,3,5,2,2,3,3,5]
+}, {
+    "name" : "Ronald Weasley",
+    "photo" : "https://imgix.bustle.com/rehost/2016/9/13/556a0b44-17b5-429c-8954-2dc926bfbc70.jpg",
+    "scores" : [3,4,1,5,2,2,2,2,4,5]
+}, {
+    "name" : "Hermione Granger",
+    "photo" : "https://imgix.bustle.com/rehost/2016/9/13/33785d76-556f-4ea4-8cf3-10d190897860.png",
+    "scores" : [5,5,5,5,3,4,5,5,1,5]
+}, {
+    "name" : "Hagrid",
+    "photo" : "https://i2.wp.com/bookstacked.com/wp-content/uploads/2016/08/HagridHeader.jpg",
+    "scores" : [5,2,2,2,4,1,1,2,5,3]
+}, {
+    "name" : "Albus Dumbledore",
+    "photo" : "https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/24/17/Albus-Dumbledore.jpg",
+    "scores" : [5,1,5,3,5,1,2,5,5,5]
+}, {
+    "name" : "Severus Snape",
+    "photo" : "https://vignette.wikia.nocookie.net/harrypotter/images/6/63/SnapeSeverus.jpg",
+    "scores" : [1,5,5,5,5,5,5,5,1,1]
+}, {
+    "name" : "Draco Malfoy",
+    "photo" : "https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Draco_Mal.JPG/220px-Draco_Mal.JPG",
+    "scores" : [1,4,2,4,2,5,5,4,1,2]
+}, {
+    "name" : "Lord Voldemort",
+    "photo" : "https://www.gannett-cdn.com/-mm-/51113c05e531144c5354e297104650e83bea2942/c=632-0-2240-1206/local/-/media/2017/02/09/USATODAY/usatsports/vol4.jpg",
+    "scores" : [5,4,5,5,4,5,5,5,1,1]
+}, {
+    "name" : "Neville Longbottom",
+    "photo" : "https://i.kinja-img.com/gawker-media/image/upload/s--Pc0jktST--/c_scale,f_auto,fl_progressive,q_80,w_800/qqbrhpzx8l7xjqeoz29a.jpg",
+    "scores" : [1,1,1,4,3,1,1,3,5,4]
+}, {
+    "name" : "Dobby",
+    "photo" : "https://metrouk2.files.wordpress.com/2015/10/dobby-2.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Hedwig",
+    "photo" : "https://pm1.narvii.com/6014/baeafb4e67fb42dedc84ff26124843fb24aca1a5_hq.jpg",
+    "scores" : [1,1,5,1,1,5,1,1,1,5]
+}, {
+    "name" : "Cedric Diggory",
+    "photo" : "https://images.pottermore.com/bxd3o8b291gf/1uZurZSTWwcC6Ww8UcQ8ws/e3ce3b91e3aa2a86ce8aaed542d62f98/CedricDiggory_WB_F4_CedricDiggoryWithHandsInPockets_Promo_080615_Port.jpg",
+    "scores" : [5,5,5,3,4,2,2,4,4,5]
+}, {
+    "name" : "The Dursleys",
+    "photo" : "http://coolspotters.com/files/photos/841240/harry-potter-and-the-chamber-of-secrets-gallery.png",
+    "scores" : [1,3,1,5,1,5,5,5,1,1]
+}];
+
+module.exports = friends;
